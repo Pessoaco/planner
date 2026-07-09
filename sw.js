c6f09b8cbb6f45893bbedd2b0dda20a23141e16b
Pessoaco <leidi.pessoaco@gmail.com>
2026-07-08 21:04:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,22 @@
-const CACHE = 'planner-v59';
+const CACHE = 'planner-v60';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
   if (e.request.url.includes('script.google.com')) return;
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request, { cache: 'no-store' }).catch(() => caches.match(e.request))
   );
 });
